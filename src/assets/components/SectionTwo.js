d02bae40c7a1ac3f2d22e9abf97a7cc372a387fe
muzman01 <supurge_man@hotmail.com>
2022-04-13 12:05:02 +0300

    testbitti

--- src/assets/components/SectionTwo.js
+++ src/assets/components/SectionTwo.js
@@ -12,7 +12,7 @@ export default function SectionTwo() {
                     <p data-aos="zoom-in-down">ReitsDAO is a service operated by Blokfield.Inc and is a decentralized real estate investment product service with real estate tokens linked to real assets on the BSC blockchain network.
                     </p>
                     <div className='gggg'>
-                    <img className="img-fluid " src={S2} alt=""  data-aos="zoom-in-up"  data-aos-delay="400" />
+                    <img className="img-fluid ggg" src={S2} alt=""  data-aos="zoom-in-up"  data-aos-delay="400" />
                     </div>
                 </div>
             </div>
@@ -20,7 +20,7 @@ export default function SectionTwo() {
             <div className="col-lg-6 mt-4 mt-lg-0"  >
                 <div className="section-two-right ps-lg-5">
                     <div className='gggg'>
-                    <img className="img-fluid " src={S1}alt=""  data-aos="zoom-in-up"  data-aos-delay="400"/>
+                    <img className="img-fluid ggg " src={S1}alt=""  data-aos="zoom-in-up"  data-aos-delay="400"/>
                     </div>
                     <p data-aos="zoom-in-down">ReitsDAO’s token economy model is designed to be very simple. ReitsDAO issues “RDF” tokens that are collateralized with real estate values, and “RDF” tokens reflect the value and revenue of all real estate owned by ReitsDAO. Therefore,
                         the value of “RDF” token can be said to be the sum of all real estate values and profits owned by ReitsDAO. ReitsDAO does not issue new tokens reflecting each real estate value and is managed only as “RDF”.</p>
