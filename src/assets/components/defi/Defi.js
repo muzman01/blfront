bf0f1282a3ac434081e49c83b7882e0b6546b7d5
muzman01 <supurge_manq@hotmail.com>
2022-04-28 10:19:05 +0300

    aaaaa

--- src/assets/components/defi/Defi.js
+++ src/assets/components/defi/Defi.js
@@ -20,7 +20,7 @@ export default function Defi() {
     const [burn,Setburn] = useState()
     const [circulating, setCirculating] = useState();
     const contractAddress='0xAA731bB4bCd8C4A69C8A86E67E50942EE243debb'
-    const tokenp =0.56335
+    const tokenp =0.59014
     ;
 
     async function connect() {
